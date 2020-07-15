5bf98235525e78a5d119a4994dfeea06e676c6fd
Oksana Kalinina <ksan4ik92@gmail.com>
2020-07-15 23:15:32 +0300

    es6

--- homework.js
+++ homework.js
@@ -1,20 +1,6 @@
 const employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
-
-// let employersNames = [];
-// for (let i = 0; i < employers.length; i++) {
-// 	if (employers[i].length > 0 && employers[i].length != '') {
-// 		employersNames.push(employers[i]);
-// 	}
-// }
-
 let employersNames = employers.filter((item) => item.length>0 && item.length != '');
-
-
-// for (const i = 0; i < employersNames.length; i++) {
-// 	employersNames[i] = employersNames[i].toLowerCase().trim();
-// }
 employersNames = employersNames.map((item) => item.toLocaleLowerCase().trim());
-
 
 const sponsors = {
     cash: [40000, 5000, 30400, 12000],
@@ -36,3 +22,45 @@ function makeBusiness([...[owner, director = 'Victor', cash, emp]]) {
     console.log('Note. Be careful with ' + sponsors.eu[0] + ". It's a huge risk.");
 }
 makeBusiness(['Sam', , money, employersNames]);
+
+
+// var employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
+
+// var employersNames = [];
+// for (var i = 0; i < employers.length; i++) {
+// 	if (employers[i].length > 0 && employers[i].length != '') {
+// 		employersNames.push(employers[i]);
+// 	}
+// }
+// for (var i = 0; i < employersNames.length; i++) {
+// 	employersNames[i] = employersNames[i].toLowerCase().trim();
+// }
+
+// var sponsors = {
+//     cash: [40000, 5000, 30400, 12000],
+//     eu: ['SRL', 'PLO', 'J&K'],
+//     rus: ['RusAuto', 'SBO']
+// };
+
+// function calcCash(own) {
+//     own = own || 0;
+//     var everyCash = Array.prototype.slice.call(arguments);
+//     var total = own;
+//     for (var i = 0; i < everyCash[1].length; i++) {
+//         total += +everyCash[1][i];
+//     }
+//     return total;
+// }
+
+// var money = calcCash(null, sponsors.cash);
+
+// function makeBusiness(owner, director, cash, emp) {
+//     director = director || 'Victor';
+//     var sumSponsors = sponsors.eu.concat(sponsors.rus, 'unexpected sponsor');
+//     console.log('We have a business. Owner: ' + owner + ', director: ' + director + '. Our budget: ' + cash + '. And our employers: ' +
+//     emp);
+//     console.log('And we have a sponsors: ');
+//     console.log.apply(null, sumSponsors);
+//     console.log('Note. Be careful with ' + sponsors.eu[0] + ". It's a huge risk.");
+// }
+// makeBusiness.apply(null, ['Sam', null, money, employersNames]);
