0b98a7eb9b9ae5e2fd04acc85c3265b0fe834c01
Oksana Kalinina <ksan4ik92@gmail.com>
2020-07-16 22:19:47 +0300

    delete variable test

--- main.js
+++ main.js
@@ -17,11 +17,8 @@ const doWork = (c) =>{
 doWork(images)
     .then ((data)=> {
         data.some((item) =>{
-            let test = true;
             item.addEventListener("error", (event)=>{
-                test = false;
                 console.error(event.target.getAttribute("src"));
-                
             });
             item.addEventListener("load", () => document.body.append(item) );
         })
